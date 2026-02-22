22dd975186545039550f45ec64fda96966f11027
superevilstockholm <balilombok11@gmail.com>
2026-02-22 16:25:47 +0700

    Created classrooms table migration and model

---
+++ app/Models/MasterData/Classroom.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom extends Model
+{
+    protected $table = 'classrooms';
+
+    protected $fillable = [
+        'name'
+    ];
+}
