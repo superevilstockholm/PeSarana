265d577ff2fb544654581239019ac6f443e8a696
superevilstockholm <balilombok11@gmail.com>
2026-02-22 13:11:26 +0700

    Created role middleware

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
 });
+
+// Protected
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        // Admin
+        Route::middleware(['role:admin'])->group(function () {
+
+        });
+        // Student
+        Route::middleware(['role:student'])->group(function () {
+
+        });
+    });
+});
