200901a79e094624b888ac5ad5e66534c16bacca
superevilstockholm <balilombok11@gmail.com>
2026-02-22 23:14:11 +0700

    Created update aspiration feedback

--- app/Http/Controllers/MasterData/AspirationFeedbackController.php
+++ app/Http/Controllers/MasterData/AspirationFeedbackController.php
@@ -47,9 +47,13 @@ class AspirationFeedbackController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, AspirationFeedback $aspirationFeedback)
+    public function update(Request $request, AspirationFeedback $aspirationFeedback): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'content' => 'required|string',
+        ]);
+        $aspirationFeedback->update(['content' => $validated['content']]);
+        return redirect()->route('dashboard.admin.master-data.aspirations.show', $aspirationFeedback->aspiration->id)->with('success', 'Berhasil memperbarui data feedback aspirasi.');
     }
 
     /**
