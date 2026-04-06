fb714e985f07b9d846531c804af9f4cf4a986b81
superevilstockholm <balilombok11@gmail.com>
2026-04-06 21:09:33 +0700

    Updated navbar component change login and signup button to user profile if authenticated

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -24,16 +24,80 @@
                     <a class="nav-link" href="#faq">FAQ</a>
                 </li>
                 <li class="nav-item d-flex align-items-center d-md-none gap-3 fw-medium">
-                    <a class="nav-link" href="{{ route('login') }}">Log In</a>
-                    |
-                    <a class="nav-link" href="{{ route('signup') }}">Sign Up</a>
+                    @if (auth()->user())
+                        <div class="dropdown w-100">
+                            <a class="d-flex align-items-center text-decoration-none dropdown-toggle gap-2 mb-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="{{ auth()->user()->profile_picture_path_url ?? asset('static/img/default-profile-picture.svg') }}" alt="avatar" width="32" height="32" class="rounded-circle">
+                                <span class="d-inline">
+                                    {{ auth()->user()->name }}
+                                </span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end shadow-sm border-0 mt-2">
+                                <li>
+                                    <a class="dropdown-item fs-09" href="{{ route('dashboard.' . auth()->user()->role->value . '.index') }}">
+                                        Dashboard
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item fs-09" href="{{ route('dashboard.profile') }}">
+                                        Profile
+                                    </a>
+                                </li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li>
+                                    <form action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button class="dropdown-item text-danger fs-09">
+                                            Logout
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+                    @else
+                        <a class="nav-link" href="{{ route('login') }}">Log In</a>
+                        |
+                        <a class="nav-link" href="{{ route('signup') }}">Sign Up</a>
+                    @endif
                 </li>
             </ul>
         </div>
         <div class="d-none d-md-flex justify-content-end w-md-25 gap-3 fs-09 fw-medium">
-            <a class="nav-link" href="{{ route('login') }}">Log In</a>
-            |
-            <a class="nav-link" href="{{ route('signup') }}">Sign Up</a>
+            @if (auth()->user())
+                <div class="dropdown">
+                    <a class="d-flex align-items-center text-decoration-none dropdown-toggle gap-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="{{ auth()->user()->profile_picture_path_url ?? asset('static/img/default-profile-picture.svg') }}" alt="avatar" width="32" height="32" class="rounded-circle">
+                        <span class="d-inline">
+                            {{ auth()->user()->name }}
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end shadow-sm border-0 mt-2">
+                        <li>
+                            <a class="dropdown-item fs-09" href="{{ route('dashboard.' . auth()->user()->role->value . '.index') }}">
+                                Dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item fs-09" href="{{ route('dashboard.profile') }}">
+                                Profile
+                            </a>
+                        </li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button class="dropdown-item text-danger fs-09">
+                                    Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @else
+                <a class="nav-link" href="{{ route('login') }}">Log In</a>
+                |
+                <a class="nav-link" href="{{ route('signup') }}">Sign Up</a>
+                @endif
+            </div>
         </div>
-    </div>
 </nav>
